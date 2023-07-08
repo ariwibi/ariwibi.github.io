48266c9fcef1a5a5f4985b56ec6dc8f434401200
ariwibi <ari.bowo.0231@gmail.com>
2023-07-08 09:46:28 +0800

    menambahkan beberapa file

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active"); 
+    menu.classList.toggle("active"); 
+});
+function reloadpage() {
+    location.reload();
+}
